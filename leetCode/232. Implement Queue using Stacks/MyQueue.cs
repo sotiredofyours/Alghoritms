8ab2f7120fa69db3068a2ddb53af8cbc3818b1e2
sotiredofyours <h8everrything@gmail.com>
2022-08-05 17:57:52 +0400

    grind 75

---
+++ leetCode/232. Implement Queue using Stacks/MyQueue.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1.leetCode._232._Implement_Queue_using_Stacks;
+
+public class MyQueue
+{
+    Stack<int> onPush;
+    Stack<int> onPop;
+    public MyQueue() {
+        onPush = new Stack<int>();
+        onPop = new Stack<int>();
+    }
+    
+    public void Push(int x) {
+        while(onPop.Count!=0){
+            onPush.Push(onPop.Pop());
+        }
+        onPush.Push(x);
+    }
+    
+    public int Pop() {
+        
+        while(onPush.Count!=0){
+            onPop.Push(onPush.Pop());
+        }
+        
+        return onPop.Pop();
+    }
+    
+    public int Peek() {
+        while(onPush.Count!=0){
+            onPop.Push(onPush.Pop());
+        }
+        return onPop.Peek();
+        
+    }
+    
+    public bool Empty() {
+        return onPush.Count == 0 && onPop.Count == 0;
+    }
+}
---
+++ leetCode/383. Ransom Note/RansomNote.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1.leetCode._383._Ransom_Note;
+
+public class RansomNote
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        char[] symbols = new char[100];
+        foreach (var x in magazine)
+        {
+            symbols[x - '0']++;
+        }
+
+        foreach (var y in ransomNote)
+        {
+            if (symbols[y - '0'] == 0) return false;
+            symbols[y - '0']--;
+        }
+
+        return true;
+    }
+}
