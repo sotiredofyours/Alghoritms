c0f393e5440f22dbaf83ef952e109b12b0a63221
sotiredofyours <h8everrything@gmail.com>
2022-08-10 20:40:31 +0400

    grind 75

---
+++ leetCode/283. Move Zeroes/MoveZero.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1.leetCode._283._Move_Zeroes;
+
+public class MoveZero
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+ 	
+        for (int i = lastNonZeroFoundAt; i < nums.Length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
