37f49a3e636d6dd459f2444783384b18d9a8cacd
sotiredofyours <h8everrything@gmail.com>
2022-08-11 20:46:14 +0400

    grind 75

---
+++ leetCode/101. Symmetric Tree/SymmetricTree.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1.leetCode._101._Symmetric_Tree;
+
+public class SymmetricTree
+{
+    public class TreeNode { 
+        public int val; 
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right; 
+        }
+    }
+    
+    public bool IsSymmetric(TreeNode root) {
+        return Helper(root.left, root.right);
+    }
+
+    public static bool Helper(TreeNode left, TreeNode right)
+    {
+        if (left is null && right is null) return true;
+        if (left is null || right is null) return false;
+        if (left.val != right.val) return false;
+        return Helper(left.left, right.left) && Helper(left.right, right.right);
+    }
+}
---
+++ leetCode/268. Missing Number/MisNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1.leetCode._268._Missing_Number;
+
+public class MisNumber
+{
+    public int MissingNumber(int[] nums) {
+        var hash = new HashSet<int>();
+        for (int i=0; i<=nums.Length; i++){
+            hash.Add(i);
+        }
+        foreach (var x in nums){
+            if (hash.Contains(x)) hash.Remove(x);
+        }
+        foreach (var res in hash){
+            return res;
+        }
+        return 0;
+    }
+}
