d9071bea675343400e4e00e3160fafb0a912fbe5
sotiredofyours <h8everrything@gmail.com>
2022-08-11 20:57:57 +0400

    grind 75

---
+++ leetCode/9. Palindrome Number/PalNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1.leetCode._9._Palindrome_Number;
+
+public class PalNumber
+{
+    public bool IsPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        var sum = 0;
+        while (x> sum){
+            sum = sum * 10 + x % 10;
+            x /= 10;
+         
+        }
+        return x == sum || x == sum/10;
+    }
+}
