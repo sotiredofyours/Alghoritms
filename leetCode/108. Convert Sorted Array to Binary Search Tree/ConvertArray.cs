f9001bed26e350a0eb997ebc8f9353f986ac4b87
sotiredofyours <h8everrything@gmail.com>
2022-08-16 13:22:59 +0400

    grind 75

---
+++ leetCode/108. Convert Sorted Array to Binary Search Tree/ConvertArray.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1.leetCode._108._Convert_Sorted_Array_to_Binary_Search_Tree;
+
+public class ConvertArray
+{
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+ }
+
+    public TreeNode SortedArrayToBST(int[] nums)
+    {
+       var mid = nums.Length / 2;
+       var root = new TreeNode(nums[mid]);
+       root.left = Helper(0, mid - 1,nums);
+       root.right = Helper(mid + 1, nums.Length - 1, nums);
+       return root;
+    }
+
+    private TreeNode Helper(int l, int r, int[] nums)
+    {
+        if (r < l) return null; 
+        var mid =l+( r - l )/ 2;
+        return new TreeNode(nums[mid], Helper(l, mid - 1, nums), Helper(mid + 1, r, nums));
+    }
+
+}
---
+++ leetCode/572. Subtree of Another Tree/Subtree.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1.leetCode._572._Subtree_of_Another_Tree;
+
+public class Subtree
+{
+      public class TreeNode {
+          public int val;
+          public TreeNode left;
+          public TreeNode right;
+          public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+     
+      public bool IsSubtree(TreeNode s, TreeNode t) {
+          if (s == null)
+              return false;
+          else if (s.val == t.val && IsSameTree(s, t))
+              return true;
+          else
+              return IsSubtree(s.left, t) || IsSubtree(s.right, t);
+      }
+    
+      private bool IsSameTree(TreeNode n1, TreeNode n2)
+      {
+          if (n1 == null && n2 == null)
+              return true;
+          else if (n1 == null && n2 != null || n1 != null && n2 == null)
+              return false;
+        
+          return n1.val == n2.val && IsSameTree(n1.left, n2.left) && IsSameTree(n1.right, n2.right);
+      }
+
+    
+  
+}
