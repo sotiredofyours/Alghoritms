ee07c61b0510fdd937f0a90e358d9dff2a10eabd
sotiredofyours <h8everrything@gmail.com>
2022-08-24 13:30:44 +0400

    tasks from codeforces

---
+++ Codeforces/Codeforces Round #816 (Div. 2)/BeautyArray.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1.Codeforces.Codeforces_Round__816__Div._2_;
+
+public class BeautyArray
+{
+    public static void Main()
+    {
+        var t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var n = int.Parse(input[0]);
+            var k = int.Parse(input[1]);
+            var b = int.Parse(input[2]);
+            var s = int.Parse(input[3]);
+        }
+    }
+}
---
+++ Codeforces/Codeforces Round #816 (Div. 2)/Xmurogruz.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1.Codeforces.Codeforces_Round__816__Div._2_;
+using System;
+
+public class Xmurogruz
+{
+    public static void Main()
+    {
+        var t = int.Parse(Console.ReadLine());
+        var finalResult = new List<string>();
+        for (int i = 0; i < t; i++)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var n = int.Parse(input[0]);
+            var m = int.Parse(input[1]);
+            var result = 0;
+            if (m == 1 && n == 1)
+            {
+                Console.WriteLine(0);
+            }
+            if (m < n) result += 2 * m - 1 + n-1;
+            else
+            {
+                result += 2 * n - 1 + m - 1;
+            }
+            finalResult.Add(result.ToString());
+        }
+
+        foreach (var x in finalResult)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
