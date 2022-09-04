070980227936aa55a98cbed706832d8e87535dae
sotiredofyours <h8everrything@gmail.com>
2022-09-04 15:42:48 +0400

    grind

---
+++ leetCode/207. Course Schedule/CourseSh.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp1.leetCode._207._Course_Schedule;
+
+public class CourseSh
+{
+   
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        int[][] matrix = new int[][numCourses]; // i -> j
+        for (int i = 0; i < numCourses; i++)
+        {
+            matrix[i] = new int[numCourses];
+        }
+        int[] indegree = new int[numCourses];
+    
+        for (int i=0; i<prerequisites.Length; i++) {
+            int ready = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            if (matrix[pre][ready] == 0)
+                indegree[ready]++; //duplicate case
+            matrix[pre][ready] = 1;
+        }
+        
+        int count = 0;
+        Queue<int> queue = new Queue<int>();
+        for (int i=0; i<indegree.Length; i++) {
+            if (indegree[i] == 0) queue.Enqueue(i);
+        }
+        while (queue.Count>0) {
+            int course = queue.Dequeue();
+            count++;
+            for (int i=0; i<numCourses; i++) {
+                if (matrix[course][i] != 0) {
+                    if (--indegree[i] == 0)
+                        queue.Enqueue(i);
+                }
+            }
+        }
+        return count == numCourses;
+        
+    }
+    
+}
---
+++ leetCode/987. Vertical Order Traversal of a Binary Tree/VerticalBT.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ConsoleApp1.leetCode._987._Vertical_Order_Traversal_of_a_Binary_Tree;
+
+public class VerticalBt
+{
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+     }
+ }
+
+ public class Solution
+ {
+     public class Point
+     {
+         public Point(int rootVal, int dy)
+         {
+             Val = rootVal;
+             Dy = dy;
+         }
+
+         public int Val { get; set; }
+         public int Dy { get; set; }
+     }
+     private SortedDictionary<int, List<Point>> storage = new SortedDictionary<int, List<Point>>();
+     public IList<IList<int>> VerticalTraversal(TreeNode root)
+     {
+         var result = new List<IList<int>>();
+        AddNode(root, 100, 0);
+
+        foreach (var level in storage)
+        {
+            var list = new List<int>();
+            foreach (var point in level.Value)
+            {
+                list.Add(point.Val);
+            }
+            result.Add(new List<int>(list));
+        }
+        result.Reverse();
+        return result;
+     }
+
+     void AddNode(TreeNode root, int dx, int dy)
+     {
+         if (root is null) return;
+         if (storage.Keys.Contains(dx))
+         {
+             storage[dx].Add(new Point(root.val, dy));
+             storage[dx] = storage[dx].OrderBy(x => x.Dy).ThenBy(x => x.Val).ToList();
+         }
+         else
+         {
+             storage.Add(dx, new List<Point>{new Point(root.val, dy)});
+         }
+         AddNode(root.left, dx+1, dy+1);
+         AddNode(root.right, dx-1, dy+1);
+     }
+ }
+}
