8645e2121a659f6bfeec49ae4245990843b5224a
sotiredofyours <h8everrything@gmail.com>
2022-09-05 09:21:06 +0400

    grind

---
+++ leetCode/429. N-ary Tree Level Order Traversal/NLevelOrder.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1.leetCode._429._N_ary_Tree_Level_Order_Traversal;
+
+public class NLevelOrder
+{
+    public class Node {
+        public int val;
+        public IList<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    private IList<IList<int>> storage = new List<IList<int>>();
+    public IList<IList<int>> LevelOrder(Node root) {
+        AddNode(root);
+        return storage;
+    }
+
+    void AddNode(Node root, int level = 0)
+    {
+        if (root is null) return;
+        if (level >= storage.Count)
+        {
+            storage.Add(new List<int>{root.val});
+        }
+        else
+        {
+            storage[level].Add(root.val);
+        }
+
+        foreach (var node in root.children)
+        {
+            AddNode(node, level+1);
+        }
+    }
+
+}
